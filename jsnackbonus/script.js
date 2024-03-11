38dc43ff67fe3499788e40a91a6016c377e75b98
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-11 18:14:04 +0100

    feat: jsnackbonus

--- jsnackbonus/script.js
+++ jsnackbonus/script.js
@@ -1,1 +1,20 @@
 // Dato un array contenente 10 numeri, stampare in console il numero più grande.
+
+const numbers = [];                                                     // Array vuoto
+
+for (let i = 0; i < 10; i++) {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;             // Genero i numeri casualmente
+  numbers.push(randomNumber);                                           // Li push nell'array
+}
+
+let max = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] > max) {                                               // Confronto ogni elemento stabilento il maggiore
+    max = numbers[i];
+  }
+}
+
+// sparo i risultati
+console.log(numbers);
+console.log(`Il numero più grande è: ${max}`);
