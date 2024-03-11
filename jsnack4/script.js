b9a1f8815af5af6f2d151e505d6eae70d69bed10
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-11 18:09:13 +0100

    feat: jsnack4

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,1 +1,16 @@
 // Calcola la somma e la media dei primi 10 numeri.
+
+let sum = 0;                        // qua ci finiscono i numeri sommati
+let count = 0;                      // qua si contano quanti numeri ci sono
+
+for (let i = 1; i <= 10; i++) {     // ciclo for che conta e somma i numeri da 1 a 10
+  sum += i;
+  count++;
+}
+
+const average = sum / count;        // calcolo della media
+
+
+// sparo i risultati
+console.log(`Somma: ${sum}`);
+console.log(`Media: ${average}`);
