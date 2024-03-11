4e6ee193128e9ea7a065cb651b51ac82ae13a2b9
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-11 14:32:28 +0100

    feat: removed css link and add jsnack3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,12 @@
 // Crea un array vuoto.
 //   Chiedi per 6 volte all’utente di inserire un numero,
 //   se è dispari inseriscilo nell’array.
+
+let numbersOdd = [];                                                // Array vuoto
+for (let i = 1; i < 7; i++) {
+  let num = parseInt(prompt(`"Inserisci un numero ${i}"`));         // Richiedo i 6 numeri
+  if (num % 2 !== 0) {                                              // Se dispari
+    numbersOdd.push(num);                                           // Inserisci nell'array
+  }
+}
+console.log(numbersOdd);
