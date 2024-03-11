45b0e9d67f97fe1632a59d1b4dd91aed2459f2cf
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-11 13:31:30 +0100

    feat: snack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,19 @@
 // Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
+// Come l'esercizio in classe di stamattina, ma senza pari o dispari
+
+let numbers = [];                                   // Array vuoto
+let count = 0;
+
+for (let i = 1; i <= 5; i++) {                      // chiedo all'utente di inserire 5 numeri
+  const num = prompt(`Inserisci il numero ${i}`);
+  if (!isNaN(num) && !numbers.includes(Number(num))) {  // controlla se il numero è unico
+    numbers.push(Number(num));                          // Aggiungo all'array
+    count++;                                            // Avanza il conteggio
+  } else {
+    i--;                                            // decrementa il contatore del ciclo for se il numero non è unico
+    alert("Devi inserire un numero unico.");
+  }
+}
+
+// stampa i risultati
+alert(`Hai inserito ${count} numeri unici: ${numbers}.`);
